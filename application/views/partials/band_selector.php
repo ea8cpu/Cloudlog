dbc5af61f7ba72e4d38d2acc11a34ac025408aca
David Díaz González <dgonzalez@suse.de>
2021-01-31 21:11:17 +0000

    Include the 11m non-ham radio band
    Although 11m is not considered an Amateur Radio Band, some radio-hams
    use it. Thus, it worth to have it available, although it could imply
    more implications like filtering at the time to import the QSOs to an
    ADIF compliant external service.
    
    More info:
    
    * https://www.adif.org/311/ADIF_311.htm (III.B.4 Band Enumeration)
    * https://github.com/magicbug/Cloudlog/discussions/853
    * https://www.hamradioforum.net/threads/8773-Hams-and-11m

--- application/views/partials/band_selector.php
+++ application/views/partials/band_selector.php
@@ -14,7 +14,7 @@ $this->load->helper('form');
 
 $default_name = 'band';
 
-$hf_bands = ['160m', '80m', '60m', '40m', '30m', '20m', '17m', '15m', '12m', '10m'];
+$hf_bands = ['160m', '80m', '60m', '40m', '30m', '20m', '17m', '15m', '12m', '11m', '10m'];
 $vhf_bands = ['6m', '4m', '2m'];
 $uhf_bands = ['70cm', '23cm', '13cm', '9cm'];
 $microwave_bands = ['6cm', '3cm'];
